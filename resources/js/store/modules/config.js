e17bb64ebdf946b0df4d681d8989c6491aae2c83
kanRMUTL <kan4227@gmail.com>
2020-02-11 15:52:58 +0700

    Add config to store

---
+++ resources/js/store/modules/config.js
@@ -0,0 +1,12 @@
+export default {
+    state: {
+        address: '101/8 ม.2 ต.ช้างเผือก อ.เมือง จ.เชียงใหม่',
+        phone: '086-4351469',
+        email: 'Bossnoppadol@gmail.com'
+    },
+    getters: {
+        address: state => state.address,
+        phone: state => state.phone,
+        email: state => state.email,
+    }
+}
