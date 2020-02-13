b29a7b8f87e88a009fd236a266f2cb13788c1e4c
kanRMUTL <kan4227@gmail.com>
2020-02-13 10:24:48 +0700

    create artisan command in routing

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,16 @@ Route::get('/checkout', function () {
 Route::get('/getproducts', 'ProductController@getProduct');
 Route::post('/createOrder', 'OrderController@createOrder');
 Route::get('/sample', 'OrderController@createOrder');
+
+// Artisan command
+Route::get('/config-clear', function () {
+    $status = Artisan::call('config:clear');
+    return '<h1>Configurations cleared</h1>';
+});
+
+Route::get('/storage-link', function () {
+    $status = Artisan::call('storage:link');
+    return '<h1>run php storage:link success</h1>';
+});
+
+
