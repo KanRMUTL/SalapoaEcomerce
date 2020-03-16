9fe0b7bef3b417bf7cb2264cd47598a21067d341
kanRMUTL <kan4227@gmail.com>
2020-03-16 21:20:55 +0700

    add id to product seeder

--- database/seeds/ProductTableSeeder.php
+++ database/seeds/ProductTableSeeder.php
@@ -10,31 +10,37 @@ class ProductTableSeeder extends Seeder
     {
         Product::insert([
             [
+                'product_id' => 1,
                 'product_name' => 'ไส้อั่ว',
                 'product_img' => '01.jpg',
                 'product_price' => '20'
             ],
             [
+                'product_id' => 2,
                 'product_name' => 'ไส้มะพร้าวหวาน',
                 'product_img' => '02.jpg',
                 'product_price' => '13'
             ],
             [
+                'product_id' => 3,
                 'product_name' => 'ไส้พุทราถั่วแดง',
                 'product_img' => '03.jpg',
                 'product_price' => '15'
             ],
             [
+                'product_id' => 4,
                 'product_name' => 'ไส้กะเพราไข่เค็ม',
                 'product_img' => '04.jpg',
                 'product_price' => '20'
             ],
             [
+                'product_id' => 5,
                 'product_name' => 'ไส้เค็ม',
                 'product_img' => '05.jpg',
                 'product_price' => '10'
             ],
             [
+                'product_id' => 6,
                 'product_name' => 'ไส้ถั่วแดง',
                 'product_img' => '06.jpg',
                 'product_price' => '10'
